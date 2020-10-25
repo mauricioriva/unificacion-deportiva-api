8ba4a9abae976786c3592bd92cc7d5e62153f838
Mauricio Riva <mauricioriva@ciencias.unam.mx>
2020-10-24 19:11:00 -0500

    initial commit

---
+++ src/main/java/com/api/unificaciondeportiva/UnificacionDeportivaApplication.java
@@ -0,0 +1,13 @@
+package com.api.unificaciondeportiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnificacionDeportivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnificacionDeportivaApplication.class, args);
+	}
+
+}
